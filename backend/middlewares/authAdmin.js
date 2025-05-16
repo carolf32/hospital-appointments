4fb35ec3c515b03b6bf5bb33889f2872c7a529a3
Carolina do Amaral <carolinafrederico32@hotmail.com>
2025-05-16 14:19:33 +0100

    backend created

---
+++ backend/middlewares/authAdmin.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+const authAdmin = async (req, res, next) => {
+  try {
+    const { atoken } = req.headers;
+    if (!atoken) {
+      return res.json({ success: false, message: "Not Authorized" });
+    }
+    const token_decode = jwt.verify(atoken, process.env.JWT_SECRET);
+    if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+      return res.json({ success: false, message: "Not Authorized" });
+    }
+    next();
+  } catch (err) {
+    console.log(err);
+    res.json({ success: false, message: err.message });
+  }
+};
+
+export default authAdmin;
--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,19 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminRoute.js";
 
+//api config
 const app = express();
 const port = process.env.PORT || 3000;
 connectDB();
 connectCloudinary();
 
+//middlewares
 app.use(express.json());
 app.use(cors());
+
+//api endpoints
+app.use("/api/admin", adminRouter);
 
 app.listen(port, () => console.log("Server started at port:" + port));
